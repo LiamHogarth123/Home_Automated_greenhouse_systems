317b82a506d6f4a1dfd1297333d708a3cf507b0b
Demo748 <40484308+Demo748@users.noreply.github.com>
2023-10-10 13:39:16 +1100

    testing DH parameters for A0509

--- Models/@A0509/A0509.m
+++ Models/@A0509/A0509.m
@@ -2,7 +2,7 @@ classdef A0509 < RobotBaseClass
     %% LinearUR5 UR5 on a non-standard linear rail created by a student
 
     properties(Access = public)              
-        plyFileNameStem = 'LinearUR5';
+        plyFileNameStem = 'A0509';
     end
     
     methods
@@ -21,24 +21,32 @@ classdef A0509 < RobotBaseClass
         function CreateModel(self)   
             % Create the UR5 model mounted on a linear rail
             
-            link(1) = Link([0      0.1599  0       -pi/2   0]);
-            link(2) = Link([0      0.1357  0.425   -pi     0]);
-            link(3) = Link([0      0.1197  0.39243 pi      0]);
-            link(4) = Link([0      0.093   0       -pi/2   0]);
-            link(5) = Link([0      0.093   0       -pi/2	0]);
-            link(6) = Link([0      0       0       0       0]);
+            link(1) = Link([0       0    0  pi/2    0]);
+            link(2) = Link([0.1     0    0  0       0]);
+            link(3) = Link([0       0    0  pi/2    0]);
+            link(4) = Link([0.1     0    0  -pi/2   0]);
+            link(5) = Link([0       0    0  pi/2	0]);
+            link(6) = Link([0.05    0    0  0       0]);
+
+            % UR3e
+            % link(1) = Link('d',0.15185,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            % link(2) = Link('d',0,'a',-0.24355,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            % link(3) = Link('d',0,'a',-0.2132,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            % link(4) = Link('d',0.13105,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            % link(5) = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            % link(6) = Link('d',	0.0921,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
             
             % Incorporate joint limits
            
             link(1).qlim = [-360 360]*pi/180;
-            link(2).qlim = [-90 90]*pi/180;
-            link(3).qlim = [-170 170]*pi/180;
+            link(2).qlim = [-360 360]*pi/180;
+            link(3).qlim = [-160 160]*pi/180;
             link(4).qlim = [-360 360]*pi/180;
             link(5).qlim = [-360 360]*pi/180;
             link(6).qlim = [-360 360]*pi/180;
         
-            link(4).offset = -pi/2;
-            link(5).offset = -pi/2;
+            % link(4).offset = -pi/2;
+            % link(5).offset = -pi/2;
             
             self.model = SerialLink(link,'name',self.name);
         end
---
+++ code/testRobot.m
@@ -0,0 +1,16 @@
+
+L1 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi])
+L2 = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi])
+L3 = Link('d',1,'a',1,'alpha',pi/2,'qlim',[-160 160]*pi/180)
+L4 = Link('d',0,'a',1,'alpha',-pi/2,'qlim',[-pi pi])
+L5 = Link('d',0,'a',1,'alpha',pi/2,'qlim',[-pi pi])
+L6 = Link('d',0,'a',1,'alpha',0,'qlim',[-pi pi])
+
+
+robot = SerialLink([L1 L2 L3 L4 L5 L6],'name','myRobot')                     % Generate the model
+workspace = [-8 8 -8 8 -4 4];                                       % Set the size of the workspace when drawing the robot        
+scale = 0.5;        
+q = zeros(1,6);                                                     % Create a vector of initial joint angles        
+robot.plot(q,'workspace',workspace,'scale',scale);                  % Plot the robot
+
+robot.teach;                              
