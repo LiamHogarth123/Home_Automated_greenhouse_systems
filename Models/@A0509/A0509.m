e17658ca9991b8f69759713c8a634ef26b163245
Demo748 <40484308+Demo748@users.noreply.github.com>
2023-10-20 22:28:36 +1100

    finalised A0509 and correct ply files orientation

--- Models/@A0509/A0509.m
+++ Models/@A0509/A0509.m
@@ -1,6 +1,5 @@
 classdef A0509 < RobotBaseClass
-    %% A0509
-
+%% A0509
     properties(Access = public)              
         plyFileNameStem = 'A0509';
     end
@@ -13,54 +12,18 @@ classdef A0509 < RobotBaseClass
 				baseTr = eye(4);				
             end
             %self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
-            
             self.PlotAndColourRobot();         
         end
 
 %% Create the robot model
         function CreateModel(self)
-            % link(1) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0);
-            % link(2) = Link('d',0,'a',2,'alpha',0,'qlim',[-pi pi],'offset',0);
-            % link(3) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-160 160]*pi/180,'offset',0);
-            % link(4) = Link('d',0,'a',1,'alpha',-pi/2,'qlim',[-pi pi],'offset',0);
-            % link(5) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0);
-            % link(6) = Link('d',1,'a',0.5,'alpha',0,'qlim',[-pi pi],'offset',0);
+            link(1) = Link('d',0.155,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0);
+            link(2) = Link('d',0,'a',0.409,'alpha',0,'qlim',[-pi pi],'offset',pi/2);
+            link(3) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-160 160]*pi/180,'offset',pi/2);
+            link(4) = Link('d',0.367,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0);
+            link(5) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',pi);
+            link(6) = Link('d',0.127,'a',0,'alpha',0,'qlim',[-pi pi],'offset',0);
 
-            % link(1) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0)
-            % link(2) = Link('d',0,'a',0.409,'alpha',0,'qlim',[-pi pi],'offset',pi/2)
-            % link(3) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-160 160]*pi/180,'offset',pi/2)
-            % link(4) = Link('d',0.367,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0)
-            % link(5) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',pi)
-            % link(6) = Link('d',0.127,'a',0,'alpha',0,'qlim',[-pi pi],'offset',0)
-
-            link(1) = Link('d',0.155,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0)
-            link(2) = Link('d',0,'a',0.409,'alpha',0,'qlim',[-pi pi],'offset',pi/2)
-            link(3) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-160 160]*pi/180,'offset',pi/2)
-            link(4) = Link('d',0.367,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',0)
-            link(5) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi],'offset',pi)
-            link(6) = Link('d',0.127,'a',0,'alpha',0,'qlim',[-pi pi],'offset',0)
-      
-
-            % UR3e
-            % link(1) = Link('d',0.15185,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            % link(2) = Link('d',0,'a',-0.24355,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
-            % link(3) = Link('d',0,'a',-0.2132,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
-            % link(4) = Link('d',0.13105,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            % link(5) = Link('d',0.08535,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
-            % link(6) = Link('d',	0.0921,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
-            
-            % Incorporate joint limits
-           
-            % link(1).qlim = [-360 360]*pi/180;
-            % link(2).qlim = [-360 360]*pi/180;
-            % link(3).qlim = [-160 160]*pi/180;
-            % link(4).qlim = [-360 360]*pi/180;
-            % link(5).qlim = [-360 360]*pi/180;
-            % link(6).qlim = [-360 360]*pi/180;
-        
-            % link(4).offset = -pi/2;
-            % link(5).offset = -pi/2;
-            
             self.model = SerialLink(link,'name',self.name);
         end
      
