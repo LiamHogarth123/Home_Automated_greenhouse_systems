15f3dd94336cbbe0ff3be3f60e0bd4e2dbb27198
LiamHogarth123 <Liam.Hogarth@student.uts.edu.au>
2023-10-26 10:59:40 +1100

    update class doc

--- code/Lab2ClassTest.m
+++ code/Lab2ClassTest.m
@@ -37,8 +37,8 @@ classdef Lab2ClassTest <handle
                 final_goal = self.Table_Position(i,:);
                 self.PlaceOnePlantOnTable(self, inital_goal,final_goal, i)
                 watering_can_position = [0.25, 0.15, 0.5];
-                self.waterPlant(self, watering_can_position, final_goal)
-                self.PlaceOnePlantOnTable(self, final_goal,inital_goal, i)
+                self.waterPlant(self, watering_can_position, final_goal, i)
+%                 self.PlaceOnePlantOnTable(self, final_goal,inital_goal, i)
 %                
             end
 
@@ -51,20 +51,8 @@ classdef Lab2ClassTest <handle
     methods(Static)
         
         function placePlants(self)
-             %these brick are in a lined up configuration 
-            
-            [f,v,data] = plyread('Plant.ply','tri');
-
-            % Scale the colours to be 0-to-1 (they are originally 0-to-255
-            vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            BrickVectCount = size(v,1);
-            hold on;
-            %place object function
-            
+            hold on; 
             for i= 1:size(self.Plant_Position, 1)
-%                 
-%                 self.Plants(i) = trisurf(f,v(:,1)+ self.Plant_Position(i,1),v(:,2)+ self.Plant_Position(i,2), v(:,3) +self.Plant_Position(i,3)...
-%                     ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
                 self.Plants(i) = PlaceObject(['Plant.ply'], [self.Plant_Position(i,:)]);
             end
             self.Watering_Can = PlaceObject(['HalfSizedRedGreenBrick.ply'], [0.25, 0.15, 0.5])
@@ -91,12 +79,66 @@ classdef Lab2ClassTest <handle
                 
               
                 
+%                 %finds the required step to animate the robot
+%                 GetplantTraj = self.Calcjtraj(self, current_position, inital_goal, 1);
+%                 inital_goal = self.robot1.model.ikcon(transl(inital_goal));
+%                 DelivPlant = self.Calcjtraj(self, inital_goal, Final_goal, 1);
+%                 Final_goal = self.robot1.model.ikcon(transl(Final_goal));
+%                 rest = self.Calcjtraj(self, Final_goal, rest_goal, 1);
+%                 Movenment = [GetplantTraj;DelivPlant;rest];
+                current_position = (self.robot1.model.getpos());
+                current_position = (self.robot1.model.fkine(current_position).t)';
+
+                
+
+                Movenment = self.CalcjtrajAttempt2(self, current_position, inital_goal, Final_goal, 1);
+
+                %loops through animation
+                for j = 1:size(Movenment,1)
+                    Plant_trajectory = Movenment(j,:)
+                    for k = 1:size(Plant_trajectory,1)
+                        x = Plant_trajectory(k,:);
+                        self.robot1.model.animate(x);
+                        pause(0.25);
+                        if j > 50 && j < 100
+                            delete(self.Plants(i))
+                            RobotEndeffector = (self.robot1.model.fkine(x).t)';
+                            self.Plants(i) = PlaceObject('Plant.ply', RobotEndeffector);                           
+                            drawnow()
+                           
+                        end
+                        j                                                                       
+                    end                                                   
+                end
+                delete(self.Plants(i))
+                self.Plants(i) = PlaceObject('Plant.ply', Final_goal);
+                delete(self.Plants(i))
+                Final_goal(3) = Final_goal(3) - offset;
+                self.Plants(i) = PlaceObject('Plant.ply', Final_goal);
+
+        end
+        
+
+
+
+        function waterPlant(self, inital_goal, Final_goal,i)
+           offset = 0;
+                
+         
+                inital_goal(3) = inital_goal(3) + offset;             
+                Final_goal(3) = Final_goal(3) + offset;
+                current_position = self.robot1.model.getpos();
+                rest_goal = Final_goal(1) -0.4;
+                
+                
+              
+                
                 %finds the required step to animate the robot
-                GetplantTraj = self.Calcjtraj(self, current_position, inital_goal, 1);
+                GetplantTraj = self.Calcjtraj(self, current_position, inital_goal, 2); %error here
                 inital_goal = self.robot1.model.ikcon(transl(inital_goal));
-                DelivPlant = self.Calcjtraj(self, inital_goal, Final_goal, 1);
+                DelivPlant = self.Calcjtraj(self, inital_goal, Final_goal, 2);
                 Final_goal = self.robot1.model.ikcon(transl(Final_goal));
-                rest = self.Calcjtraj(self, Final_goal, rest_goal, 1);
+                rest = self.Calcjtraj(self, Final_goal, rest_goal, 2);
                 Movenment = [GetplantTraj;DelivPlant;rest];
          
 
@@ -109,116 +151,21 @@ classdef Lab2ClassTest <handle
                         pause(0.25);
                         if j > 50 && j < 100
                             delete(self.Plants(i))
-                            [f,v,data] = plyread('Plant.ply','tri');
-                            
-                            % Scale the colours to be 0-to-1 (they are originally 0-to-255
-                            vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-                            BrickVectCount = size(v,1);
-                            hold on;
-                            %place object function
-                                        
-                                        
-                                             
-                            
-
-                            RobotEndeffector = self.robot1.model.fkine(x).t;
-                            RobotEndeffector = RobotEndeffector';
-                            self.Plants(i) = PlaceObject('Plant.ply', RobotEndeffector);
-                           
+                            RobotEndeffector = (self.robot1.model.fkine(x).t)';
+                            self.Plants(i) = PlaceObject('Plant.ply', RobotEndeffector);                           
                             drawnow()
-                            
+                           
                         end
-                        j
-                      
-                        
-                          
-                    end
-                   
-                    
-
-              
+                        j                                                                       
+                    end                                                   
                 end
                 delete(self.Plants(i))
-                self.Plants(i) = PlaceObject('Plant.ply', [Final_goal]);
-                
-
-            % ---------------------------------------------------------------------------------------
-                
-                
-                 
-                [f,v,data] = plyread('Plant.ply','tri');
-                vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+                self.Plants(i) = PlaceObject('Plant.ply', Final_goal);
+                delete(self.Plants(i))
                 Final_goal(3) = Final_goal(3) - offset;
-                self.Plants(i) = trisurf(f,v(:,1)+ Final_goal(1),v(:,2)+ Final_goal(2), v(:,3) +Final_goal(3)...
-                    ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+                self.Plants(i) = PlaceObject('Plant.ply', Final_goal);
+
         end
-        
-
-
-
-        function waterPlant(self, inital_goal, Final_goal)
-            %% placing plants on table
-                offset = 0;
-                steps = 50;
-           
-                inital_goal(3) = inital_goal(3) + offset;
-        
-                %reads the current position
-                current_position = self.robot2.model.getpos();
-                
-                %calculates the required translation
-                requiredTranslation = transl(inital_goal) * troty(90,'deg')* trotx(90,'deg');
-        
-                %caclutes the required q angles for the final position
-                PlantInverseKnimatics = self.robot2.model.ikcon(requiredTranslation, current_position);
-                
-                %finds the required step to animate the robot
-                Plant_trajectory = jtraj(current_position, PlantInverseKnimatics, steps);
-                
-                %loops through animation
-                for j = 1:size(Plant_trajectory,1)
-                    x = Plant_trajectory(j,:);
-                    self.robot2.model.animate(x);
-                    pause(0.1);
-              
-                end
-%                 delete(self.Plants(i))
-
-            % ---------------------------------------------------------------------------------------
-                %This section deliever the plant to the table position
-
-                %updates the current position
-                current_position = self.robot1.model.getpos();
-
-                Final_goal(3) = Final_goal(3) + offset;
-        
-                %calculate the translation, invserse kinematics, and trajectory
-                requiredTranslation = transl(Final_goal) * troty(90,'deg')*trotx(90,'deg');
-                PlantInverseKnimatics = self.robot1.model.ikcon(requiredTranslation, current_position);
-                Brick_trajectory = jtraj(current_position, PlantInverseKnimatics, steps);
-                
-                %loops through the animation
-                for k = 1:size(Brick_trajectory,1)
-                    x = Brick_trajectory(k,:);
-                    self.robot1.model.animate(x);
-                    pause(0.25);
-
-%                     delete(self.Plants(i))
-                 
-                    drawnow();  
-                
-
-          
-                end
-                
-                 
-                [f,v,data] = plyread('HalfSizedRedGreenBrick.ply','tri');
-                vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-                Final_goal(3) = Final_goal(3) - offset;
-                self.Watering_Can = trisurf(f,v(:,1)+ Final_goal(1),v(:,2)+ Final_goal(2), v(:,3) +Final_goal(3)...
-                    ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
-            end
-
 
         
 
@@ -267,6 +214,31 @@ classdef Lab2ClassTest <handle
                     PlantInverseKnimatics = self.robot1.model.ikcon(requiredTranslation, start);
                 end
                 trajectory = jtraj(start, PlantInverseKnimatics, steps);
+                
+            end
+
+            function [trajectory] = CalcjtrajAttempt2(self, CurrentPosition, start, finish, robot)
+                rest = finish;
+                rest(3) = rest(3)+0.5;
+                steps = 50;
+                if robot == 2
+                    
+                    qinital = self.robot2.model.ikcon(transl(CurrentPosition)*trotx(pi)); % uses ikcon to get joint angles and prevent collision. multiplied by trotx(pi) to rotate end-effector
+                    qstart = self.robot2.model.ikcon(transl(start)*trotx(pi)); % uses ikcon to get joint angles and prevent collision. multiplied by trotx(pi) to rotate end-effector
+                    qfinish = self.robot2.model.ikcon(transl(finish)*trotx(pi));
+                    qrest = self.robot2.model.ikcon(transl(rest)*trotx(pi));                   
+                    
+                else
+                    qinital = self.robot1.model.ikcon(transl(CurrentPosition)*trotx(pi)); % uses ikcon to get joint angles and prevent collision. multiplied by trotx(pi) to rotate end-effector
+                    qstart = self.robot1.model.ikcon(transl(start)*trotx(pi)); % uses ikcon to get joint angles and prevent collision. multiplied by trotx(pi) to rotate end-effector
+                    qfinish = self.robot1.model.ikcon(transl(finish)*trotx(pi));
+                    qrest = self.robot1.model.ikcon(transl(rest)*trotx(pi));
+
+                end
+                Jtraj_inital_start = jtraj(qinital,qstart,steps);
+                Jtraj_start_finish = jtraj(qstart,qfinish,steps);
+                Jtraj_finish_rest = jtraj(qfinish,qrest,steps);
+                trajectory = [Jtraj_inital_start; Jtraj_start_finish; Jtraj_finish_rest]; % adding all joint angles to a single qMatrix
                 
             end
 
